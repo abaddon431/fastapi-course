3041b34cf979c11b93d944a6695192b5cdfd461d
Bryan Tabares <tabaresbryan39@gmail.com>
2024-03-01 14:04:43 +0800

    used http 401 instead of 403 for wrong login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -18,8 +18,8 @@ def login(user_creds: OAuth2PasswordRequestForm = Depends(), db: Session = Depen
         models.Users.email == user_creds.username
     ).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Invalid Credentials")
     if not verify(user_creds.password, user.password) :
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials")
     access_token = oauth2.create_access_token(data={"user_id":user.id})
     return {"access_token":access_token, "token_type":"bearer"}
